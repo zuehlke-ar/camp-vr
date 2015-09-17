cfa0a1acc733d7807bafe280f9948a09f1141584
brookman <rc-p90@bluemail.ch>
2015-09-17 18:37:45 +0200

    worked

---
+++ unity/RailProfile/Assets/ExtrudeScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExtrudeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+
+
+		foreach (Vector3 vertex in mesh.vertices) {
+			Debug.Log (vertex.x+" "+vertex.y+" "+vertex.z);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
